3b722a9b29eb00de93616e542ac2b84d3bc0b3b7
snoutandsnug <snoutandsnug@gmail.com>
2026-08-17 15:46:29 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,7 @@
-const CACHE="holton-homes-os-v13-2026-08-16";
+const CACHE="holton-homes-os-clean-ai-addon-2026-08-17";
 const ASSETS=[
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./v13.css",
-  "./app.js",
-  "./cloud-config.js",
-  "./manifest.webmanifest",
-  "./assets/Holton-Homes-Primary-Full-Color.svg"
+  "./","./index.html","./styles.css","./v13.css","./holton-ai.css","./app.js","./holton-ai.js","./cloud-config.js","./manifest.webmanifest","./assets/Holton-Homes-Primary-Full-Color.svg","./pip-default.webp","./pip-thinking.webp","./pip-celebrate.webp","./pip-work.webp","./pip-concerned.webp"
 ];
-
-self.addEventListener("install",event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener("activate",event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));
-  self.clients.claim();
-});
-
-self.addEventListener("fetch",event=>{
-  if(event.request.method!=="GET")return;
-  event.respondWith(
-    fetch(event.request)
-      .then(response=>{
-        if(response.ok&&new URL(event.request.url).origin===self.location.origin){
-          const copy=response.clone();
-          caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-        }
-        return response;
-      })
-      .catch(()=>caches.match(event.request).then(response=>response||caches.match("./index.html")))
-  );
-});
+self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
+self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
+self.addEventListener("fetch",event=>{if(event.request.method!=="GET")return;event.respondWith(fetch(event.request).then(response=>{if(response.ok&&new URL(event.request.url).origin===self.location.origin){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{})}return response}).catch(()=>caches.match(event.request).then(response=>response||caches.match("./index.html"))))});
